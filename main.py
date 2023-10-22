4b0c7cb9ba0ed0c42402f85a0861c6ae0be2973c
aienmii <bohatyranastasiia@gmail.com>
2023-10-22 16:05:56 +0200

    add construction to write out phone number,and find from each country is it

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import  phonenumbers
+from number import number
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from phonenumbers import  geocoder
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
